347579a9a5feab5f33a7883f75b0d2522c6aff85
HUAWEI <mve9031177286@gmail.com>
2021-04-19 17:14:15 +0300

    Тестовое задание.Убрал принт.

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,7 @@ public class Main {
         Cat bigCat = new Cat(5500, "Murka", 2, "Black");
         Cat smallCat = new Cat(1000, "Kuzya", 1, "White");
         bigCat.setName("Pushok");
-        System.out.println("The name of big cat is " + bigCat.getName());
-        System.out.println("The name of small cat is " + smallCat.getName());
-        System.out.println(Cat.catCount);
+        
         }
 }
 
